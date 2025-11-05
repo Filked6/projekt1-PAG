c867aa5e3907ae6c56423f75f6b4faf4d827bfb5
Filked6 <167993905+Filked6@users.noreply.github.com>
2025-11-05 11:51:07 +0100

    Rysowanie drogi na mapie

--- View.py
+++ View.py
@@ -30,8 +30,8 @@ def NX_visualisation(g: Graph):
     plt.axis('equal')
     plt.show()
 
-# Wyświetlanie w html (weź to okomentuj Filip jakoś)
-def Web_visualisation(g: Graph):
+# Wyświetlanie w html 
+def Web_visualisation(g: Graph, path):
     transformer = Transformer.from_crs("EPSG:2180", "EPSG:4326", always_xy=True)
 
     # Transformacja współrzędnych na WGS 84
@@ -39,32 +39,56 @@ def Web_visualisation(g: Graph):
     for node_id, node in g.nodes.items():
             lon, lat = transformer.transform(node.x, node.y)
             vertex_coords_4326[node_id] = (lon, lat)
-
+    
     min_lat = min(v[1] for v in vertex_coords_4326.values())
     max_lat = max(v[1] for v in vertex_coords_4326.values())
     min_lon = min(v[0] for v in vertex_coords_4326.values())
     max_lon = max(v[0] for v in vertex_coords_4326.values())
-
+    
+    #Obliczanie środka dla całego obszaru
     lat = (min_lat + max_lat) / 2
     lon = (min_lon + max_lon) / 2
 
     place_coords_latlon = [lat, lon]
+    #Tworzenie mapy
     m = folium.Map(location=place_coords_latlon, zoom_start=15) # Zoom trzeba zautomatyzować
+    #Punty dla początka i końca w celu wyświetlenia markerów
+    start_id = path[0].id
+    end_id = path[-1].id
 
+    lon_start, lat_start = vertex_coords_4326[start_id]
+    lon_end, lat_end = vertex_coords_4326[end_id]
+    location_start = [lat_start, lon_start]
+    location_end = [lat_end, lon_end]
+    
+    #Rysowanie linii trasy
     for e in g.edges.values():
-        id1 = e.start.id
-        id2 = e.end.id
+        id1 = e.start
+        id2 = e.end
 
-        lon1, lat1 = vertex_coords_4326[id1]
-        lon2, lat2 = vertex_coords_4326[id2]
+        lon1, lat1 = vertex_coords_4326[id1.id]
+        lon2, lat2 = vertex_coords_4326[id2.id]
+        if id1 in path and id2 in path:
+            folium.PolyLine(
+                locations=[[lat1, lon1], [lat2, lon2]],
+                color="red",
+                weight=5,
+                opacity=1
+            ).add_to(m)
 
-        folium.PolyLine(
-            locations=[[lat1, lon1], [lat2, lon2]],
-            color="gray",
-            weight=3,
-            opacity=0.7
-        ).add_to(m)
+    #Rysowanie markerów początka i końca
+    folium.Marker(
+        location=location_start,
+        tooltip="Start",
+        icon=folium.Icon(color='black', icon='flag', prefix='fa')
+    ).add_to(m)
 
+    folium.Marker(
+        location=location_end,
+        tooltip="Koniec",
+        icon=folium.Icon(color='blue', icon='flag', prefix='fa')
+    ).add_to(m)
+    """
     for vertex_id, (lon, lat) in vertex_coords_4326.items():
         folium.CircleMarker(
             location=[lat, lon],
@@ -75,6 +99,7 @@ def Web_visualisation(g: Graph):
             fill_opacity=1.0,
             popup=f"ID: {vertex_id}"
         ).add_to(m)
-
+    """
+    #Zapis i otwarcie w przeglądarce
     m.save("mapka.html")
     webbrowser.open_new_tab("mapka.html")
